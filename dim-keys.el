f2405b5e2ef3a3dd069cf2ddfffa4dff13183955
Dimitri Fontaine <dim@tapoueh.org>
2009-07-15 09:52:03 +0100

    alternative buffer scrolling

--- dim-keys.el
+++ dim-keys.el
@@ -110,4 +110,11 @@ vi style of % jumping to matching brace."
 
 (global-set-key (kbd "C-c m") 'dim:insert-previous-message)
 
+(require 'buffer-move)
+(global-set-key (kbd "<C-S-up>")     'buf-move-up)
+(global-set-key (kbd "<C-S-down>")   'buf-move-down)
+(global-set-key (kbd "<C-S-left>")   'buf-move-left)
+(global-set-key (kbd "<C-S-right>")  'buf-move-right)
+
+
 (provide 'dim-keys)
