0a46bc4f3b34de3ee38c8b782c3d7d7ab57f4831
Dimitri Fontaine <dim@tapoueh.org>
2009-07-21 19:07:31 +0100

    C-c m now takes C-u N to get nth previous-line

--- dim-keys.el
+++ dim-keys.el
@@ -92,19 +92,21 @@ vi style of % jumping to matching brace."
 
 ;; print last message
 ;; current-message is already lost by the time this gets called
-
-(defun dim:previous-message ()
+(defun dim:previous-message (&optional nth)
   "get last line of *Message* buffer"
   (with-current-buffer (get-buffer "*Messages*")
     (save-excursion
       (goto-char (point-max))
-      (previous-line)
+      (setq nth (if nth nth 1))
+      (while (> nth 0)
+	(previous-line)
+	(setq nth (- nth 1)))
       (buffer-substring (line-beginning-position) (line-end-position)))))
 
-(defun dim:insert-previous-message ()
+(defun dim:insert-previous-message (&optional nth)
   "insert last message of *Message* to current position"
-  (interactive)
-  (insert (format "%s" (dim:previous-message))))
+  (interactive "p")
+  (insert (format "%s" (dim:previous-message nth))))
 
 (global-set-key (kbd "C-c m") 'dim:insert-previous-message)
 
