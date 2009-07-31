727502ab39138f62b52d5507d0c6f7aad609e9b8
Dimitri Fontaine <dim@tapoueh.org>
2009-07-31 21:54:28 +0100

    C-S-d is duplicate-current-line

--- dim-keys.el
+++ dim-keys.el
@@ -117,4 +117,22 @@ vi style of % jumping to matching brace."
 (global-set-key (kbd "<C-S-right>")  'buf-move-right)
 
 
+;; duplicate current line
+(defun duplicate-current-line (&optional n)
+  "duplicate current line, make more than 1 copy given a numeric argument"
+  (interactive "p")
+  (save-excursion
+    (let ((nb (or n 1))
+	  (current-line (thing-at-point 'line)))
+      ;; when on last line, insert a newline first
+      (when (or (= 1 (forward-line 1)) (eq (point) (point-max)))
+	(insert "\n"))
+
+      ;; now insert as many time as requested
+      (while (> n 0)
+	(insert current-line)
+	(setq n (- n 1))))))
+
+(global-set-key (kbd "C-S-d") 'duplicate-current-line)  
+
 (provide 'dim-keys)
