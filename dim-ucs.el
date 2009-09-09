6af101de0f19452cf37eed06f19f3395f4f1d747
Dimitri Fontaine <dim@tapoueh.org>
2009-09-09 19:57:17 +0100

    Setup gnus 1@@home

---
+++ dim-ucs.el
@@ -0,0 +1,77 @@
+(defvar dim:ucs-croche-1 "♪"
+  "From http://unicode.org/charts/PDF/U2600.pdf --- code point 266A")
+
+(defvar dim:ucs-croche-2 "♫"
+  "From http://unicode.org/charts/PDF/U2600.pdf --- code point 266B")
+
+(defvar dim:ucs-double-croche "♬"
+  "From http://unicode.org/charts/PDF/U2600.pdf --- code point 266C")
+
+(defvar dim:ucs-bemol "♭"
+  "From http://unicode.org/charts/PDF/U2600.pdf --- code point 266D")
+
+(defvar dim:ucs-becare "♮"
+  "From http://unicode.org/charts/PDF/U2600.pdf --- code point 266E")
+
+(defvar dim:ucs-diese "♯"
+  "From http://unicode.org/charts/PDF/U2600.pdf --- code point 266F")
+
+(defvar dim:ucs-gear "☼"
+  "From http://unicode.org/charts/PDF/U2600.pdf --- code point 263C")
+
+(defvar dim:ucs-sun-black "☀"
+  "From http://unicode.org/charts/PDF/U2600.pdf --- code point 2600")
+
+(defvar dim:ucs-yin-yang "☯"
+  "From http://unicode.org/charts/PDF/U2600.pdf --- code point 262F")
+
+(defvar dim:ucs-peace-love "☮"
+  "From http://unicode.org/charts/PDF/U2600.pdf --- code point 262E")
+
+(defvar dim:ucs-circle-point "☉"
+  "From http://unicode.org/charts/PDF/U2600.pdf --- code point 2609")
+
+(defvar dim:ucs-check-mark "✓"
+  "From http://unicode.org/charts/PDF/U2700.pdf --- code point 2713")
+
+(defvar dim:ucs-heavy-check-mark "✔"
+  "From http://unicode.org/charts/PDF/U2700.pdf --- code point 2714")
+
+(defvar dim:ucs-ballot "✗"
+  "From http://unicode.org/charts/PDF/U2700.pdf --- code point 2717")
+
+(defvar dim:ucs-heavy-ballot "✘"
+  "From http://unicode.org/charts/PDF/U2700.pdf --- code point 2718")
+
+(defvar dim:ucs-enveloppe "✉"
+  "From http://unicode.org/charts/PDF/U2700.pdf --- code point 2709")
+
+(defvar dim:ucs-telephone "✆"
+  "From http://unicode.org/charts/PDF/U2700.pdf --- code point 2706")
+
+(defvar dim:ucs-white-florette "❀"
+  "From http://unicode.org/charts/PDF/U2700.pdf --- code point 2740")
+
+(defvar dim:ucs-black-florette "❁"
+  "From http://unicode.org/charts/PDF/U2700.pdf --- code point 2741")
+
+(defvar dim:ucs-black-diamond-white-x "❖"
+  "From http://unicode.org/charts/PDF/U2700.pdf --- code point 2756")
+
+(defvar dim:ucs-circle-white-star "✪"
+  "From http://unicode.org/charts/PDF/U2700.pdf --- code point 272A")
+
+(defvar dim:ucs-black-4-pointed-star "✦"
+  "From http://unicode.org/charts/PDF/U2700.pdf --- code point 2726")
+
+(defvar dim:ucs-white-4-pointed-star "✧"
+  "From http://unicode.org/charts/PDF/U2700.pdf --- code point 2727")
+
+(defvar dim:ucs-open-center-black-star "✫"
+  "From http://unicode.org/charts/PDF/U2700.pdf --- code point 272B")
+
+(defvar dim:ucs-outlined-black-star "✭"
+  "From http://unicode.org/charts/PDF/U2700.pdf --- code point 272D")
+
+(defvar dim:ucs-circled-ring-operator "⊚"
+  "From http://unicode.org/charts/PDF/U2700.pdf --- code point 229A")
