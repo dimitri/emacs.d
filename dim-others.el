2769c1e106d97c120779dc7072cb1f1270f0876b
Dimitri Fontaine <dim@tapoueh.org>
2009-09-24 16:00:20 +0100

    dim-others for stuff I don't have a use for, yet.

---
+++ dim-others.el
@@ -0,0 +1,13 @@
+;; no provide, not required, random functions and pastes
+
+(defun twinkle-call-symbol-orregion ()
+  "Call the phone number at point (symbol seems good enough), or in region"
+  (interactive)
+  (shell-command-to-string 
+   (format "twinkle --cmd 'call %s'"
+	   (replace-regexp-in-string 
+	    " " "" 
+	    (if (use-region-p)
+		(buffer-substring (region-beginning) (region-end))
+	      (thing-at-point 'symbol))))))
+
