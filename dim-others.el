abdfb3590a6181b5c848a080f10ac30788d120d3
Dimitri Fontaine <dim@tapoueh.org>
2009-09-28 09:46:21 +0100

    twinkle-call-symbol-or-region

--- dim-others.el
+++ dim-others.el
@@ -1,12 +1,12 @@
 ;; no provide, not required, random functions and pastes
 
-(defun twinkle-call-symbol-orregion ()
+(defun twinkle-call-symbol-or-region ()
   "Call the phone number at point (symbol seems good enough), or in region"
   (interactive)
   (shell-command-to-string 
    (format "twinkle --cmd 'call %s'"
 	   (replace-regexp-in-string 
-	    " " "" 
+	    "[^0-9+]" "" 
 	    (if (use-region-p)
 		(buffer-substring (region-beginning) (region-end))
 	      (thing-at-point 'symbol))))))
