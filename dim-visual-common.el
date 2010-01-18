62b422bdcb3784eacd4703381da78ca6a0f5e150
Dimitri Fontaine <dim@tapoueh.org>
2010-01-18 10:11:22 +0100

    Extend (get-screen-dimensions) to support X-window-system too.

--- dim-visual-common.el
+++ dim-visual-common.el
@@ -20,20 +20,30 @@
 (require 'woman)
 (setq woman-use-own-frame nil)
 
-;; osascript -e 'tell application \"Finder\" to get bounds of window of desktop'
-;; "0, 0, 2560, 1440\n"
 (defun get-screen-dimensions ()
-  "get dimencions of current screen, using osascript or xdpyinfo depending on system"
+  "get dimensions of current screen, support X window system and macosx"
 
   (cond
    ((string-match "apple-darwin" system-configuration)
-
+    ;; osascript -e 'tell application \"Finder\" to get bounds of window of desktop'
+    ;; "0, 0, 2560, 1440\n"
     (mapcar 
      'string-to-number
      (split-string 
       (substring
        (shell-command-to-string
 	"osascript -e 'tell application \"Finder\" to get bounds of window of desktop'") 6 -1) ", ")))
+
+   ((eq window-system 'x)
+    ;; dim ~ xwininfo -root -metric | awk -F '[ x+]' '/geometry/ {print $4, $5}'
+    ;; 1680 1050
+    (mapcar
+     'string-to-number
+     (split-string
+      (substring
+       (shell-command-to-string 
+	"xwininfo -root -metric | awk -F '[ x+]' '/geometry/ {print $4, $5}'")
+       0 -1) " ")))
 
   ;; linux version is yet to write
   (t '(0 0))))
