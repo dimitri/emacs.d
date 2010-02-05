ebf876bd7e65884409db25f6c7eeeeb60b398076
Dimitri Fontaine <dim@DimMBP-2.local>
2010-02-05 14:25:26 +0100

    Fix get-domain-name for when there's no match

--- dim-projects.el
+++ dim-projects.el
@@ -4,12 +4,14 @@
 (defun get-domain-name (&optional from)
   "Returns the system domain name.  If FROM is 'resolv or nil,
 returns the value defined in /etc/resolv.conf."
-  (when (or (null from) (eq from 'resolv))
-    (when (file-readable-p "/etc/resolv.conf")
-      (with-temp-buffer
-	(insert-file-contents-literally "/etc/resolv.conf")
-	(when (re-search-forward "^domain \\([^ ]+\\)$" nil t)
-	  (match-string 1))))))
+  (or
+   (when (or (null from) (eq from 'resolv))
+     (when (file-readable-p "/etc/resolv.conf")
+       (with-temp-buffer
+	 (insert-file-contents-literally "/etc/resolv.conf")
+	 (when (re-search-forward "^domain \\([^ ]+\\)$" nil t)
+	   (match-string 1))))))
+  "")
 
 (if (string-match "hi-media-techno" (get-domain-name))
     (require 'dim-projects-hm)
