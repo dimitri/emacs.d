a560928964005b66428a8ee0dbea257925702baa
Dimitri Fontaine <dim@tapoueh.org>
2010-09-02 21:19:39 +0200

    Remove duplicates in `el-get-sources'

--- dim-packages.el
+++ dim-packages.el
@@ -26,7 +26,7 @@
  (setq magit-git-executable "/usr/local/git/bin/git"))
 
 (setq el-get-sources
-      '(cssh el-get switch-window vkill google-maps nxhtml xcscope yasnippet
+      '(cssh el-get switch-window vkill google-maps yasnippet
 
 	(:name magit 
 	       :after (lambda () (global-set-key (kbd "C-x C-z") 'magit-status)))
