f6be17bcdcc91b2560527afcc20440fa8ee0ca67
Dimitri Fontaine <dim@tapoueh.org>
2010-09-04 22:33:15 +0200

    (setq offlineimap-enable-mode-line-p '(member major-mode '(gnus-group-mode gnus-summary-mode)))

--- dim-packages.el
+++ dim-packages.el
@@ -108,6 +108,8 @@
 
 	(:name offlineimap
 	       :after (lambda ()
+			(setq offlineimap-enable-mode-line-p 
+			      '(member major-mode '(gnus-group-mode gnus-summary-mode)))
 			(set-face-attribute 'offlineimap-msg-syncingfolders-face nil :foreground "DarkGoldenrod")
 			(set-face-attribute 'offlineimap-msg-skippingfolder-face nil :foreground "DarkGoldenrod")
 			(require 'gnus)
