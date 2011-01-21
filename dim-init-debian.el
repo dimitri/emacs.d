edaefecc0a35e158423d2214ec41eb26e274acef
Dimitri Fontaine <dim@tapoueh.org>
2011-01-21 11:47:03 +0100

    Don't try adapting the font unless in 'x window-system

--- dim-init-debian.el
+++ dim-init-debian.el
@@ -5,13 +5,13 @@
 (when (eq window-system 'x)
   (require 'color-theme)
   (require 'color-theme-tango)
-  (color-theme-tango))
+  (color-theme-tango)
 
-;; be easy on the eyes
-;; (if (equal (get-screen-dimensions) '(1440 900))
-;;     (set-face-font 'default "Monospace-10")
-;;   (set-face-font 'default "Monospace-12"))
+  ;; be easy on the eyes
+  ;; (if (equal (get-screen-dimensions) '(1440 900))
+  ;;     (set-face-font 'default "Monospace-10")
+  ;;   (set-face-font 'default "Monospace-12"))
 
-(set-face-font 'default "Monospace-10")
+  (set-face-font 'default "Monospace-10"))
 
 (provide 'dim-init-debian)
