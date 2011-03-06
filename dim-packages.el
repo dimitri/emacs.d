4b110f4d595b12135bf4db5b05588a9f53639e84
Dimitri Fontaine <dim@tapoueh.org>
2011-03-06 14:15:01 +0100

    Autoload doc-mode for .asciidoc files.

--- dim-packages.el
+++ dim-packages.el
@@ -42,6 +42,7 @@
 	       :after (lambda ()
 			(autoload 'doc-mode "doc-mode" nil t)
 			(add-to-list 'auto-mode-alist '("\\.adoc$" . doc-mode))
+			(add-to-list 'auto-mode-alist '("\\.asciidoc$" . doc-mode))
 			(add-hook 'doc-mode-hook '(lambda ()
 						    (turn-on-auto-fill)
 						    (require 'asciidoc)))))
