967ef6d17de09c21077d21ef4919d34b0c7a70d8
Dimitri Fontaine <dim@tapoueh.org>
2011-05-26 09:46:19 +0200

    ESC M-x (translated from <escape> M-x) runs the command execute-extended-command

--- dim-packages.el
+++ dim-packages.el
@@ -38,6 +38,7 @@
         (:name smex
 	       :after (lambda ()
 			(setq smex-save-file "~/.emacs.d/.smex-items")
+			(global-set-key (kbd "ESC M-x") 'execute-extended-command)
 			(global-set-key (kbd "M-x") 'smex)
 			(global-set-key (kbd "M-X") 'smex-major-mode-commands)))
 
