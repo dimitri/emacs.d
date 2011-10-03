1be2036451dc182a35f21d4beaf95a042e5623ee
Dimitri Fontaine <dim@tapoueh.org>
2011-10-03 15:19:34 +0200

    Implement thing-at-point 'phone-number for twinkle, thanks to jd.

--- dim-others.el
+++ dim-others.el
@@ -1,16 +1,33 @@
 ;;; dim-others.el --- Dimitri Fontaine
 ;; no provide, not required, random functions and pastes
 
+(defun phone-number-bounds-of-phone-number-at-point ()
+  "Return the start and end points of a phone number at the current point.
+The result is a paired list of character positions for an phone
+number located at the current point in the current buffer. An
+phone number is any decimal digit 0 through 9 with an optional
+starting plus symbol (`+') and with `.', `-' or space in it."
+  (save-excursion
+    (skip-chars-backward "0123456789 .+-")
+    (if (looking-at "[+0-9 ]+")
+        (let ((start (point)))
+          (skip-chars-forward "0123456789 .+-")
+          (cons start (point)))
+      nil)))
+
+(put 'phone-number 'bounds-of-thing-at-point
+     'phone-number-bounds-of-phone-number-at-point)
+
 (defun twinkle-call-symbol-or-region ()
   "Call the phone number at point (symbol seems good enough), or in region"
   (interactive)
-  (shell-command-to-string 
+  (shell-command-to-string
    (format "twinkle --cmd 'call %s'"
-	   (replace-regexp-in-string 
-	    "[^0-9+]" "" 
+	   (replace-regexp-in-string
+	    "[^0-9+]" ""
 	    (if (use-region-p)
 		(buffer-substring (region-beginning) (region-end))
-	      (thing-at-point 'symbol))))))
+	      (thing-at-point 'phone-number))))))
 
 ;; on request by cyrilb, who missed it from vim
 ;; no global-set-key yet, still have to think I'll use it someday...
