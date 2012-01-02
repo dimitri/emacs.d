850a1494a6df7144c748f2db8054afb2461167a8
Dimitri Fontaine <dim@tapoueh.org>
2012-01-02 15:52:35 +0100

    Add PartMgr to the project list.

--- dim-projects-hm.el
+++ dim-projects-hm.el
@@ -55,7 +55,8 @@
    ("pgfincore"         . "~/PostgreSQL/pgfoundry/pgfincore")
    ("repmgr"            . "~/PostgreSQL/pgfoundry/repmgr")
    ("apt.pg"            . "~/PostgreSQL/pgfoundry/apt.postgresql.org")
-   ("myfdw"            . "~/PostgreSQL/pgfoundry/mysql_fdw")
+   ("myfdw"             . "~/PostgreSQL/pgfoundry/mysql_fdw")
+   ("PartMgr"           . "~/PostgreSQL/pgfoundry/PartMgr")
 
    ;; emacs related projects
    ("cssh"		. "~/dev/emacs/cssh")
