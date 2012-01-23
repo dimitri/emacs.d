ab00b8d9ff4a6d59c9c2b767f36452deb99d6e90
Dimitri Fontaine <dim@tapoueh.org>
2012-01-23 13:47:17 +0100

    Back to color-theme-tango!

--- dim-init-debian.el
+++ dim-init-debian.el
@@ -5,20 +5,23 @@
 (when (eq window-system 'x)
   ;; switched from tango to solarized --- http://ethanschoonover.com/solarized
   ;;
-  ;; (setq color-theme-libraries nil)
-  ;; (require 'color-theme)
-  ;; (require 'color-theme-tango)
-  ;; (color-theme-tango)
+  (progn
+    (setq color-theme-libraries nil)
+    (require 'color-theme)
+    (require 'color-theme-tango)
+    (color-theme-tango)
+    (set-face-attribute 'hl-line nil :background "chocolate4"))
 
   ;; (require 'naquadah-theme)
 
-  ;; (el-get 'sync 'color-theme-solarized)
-  ;; (color-theme-solarized 'dark)
-  (color-theme-solarized 'light)
+  ;; (progn
+  ;;   (el-get 'sync 'color-theme-solarized)
+  ;;   (color-theme-solarized 'dark)
+  ;;   (color-theme-solarized 'light)
 
-  ;; this theme sets the hl-line and the transient-mark-mode background
-  ;; faces the same, which is not cool.
-  (set-face-attribute 'hl-line nil :background "grey90")
+  ;;   this theme sets the hl-line and the transient-mark-mode background
+  ;;   faces the same, which is not cool.
+  ;;   (set-face-attribute 'hl-line nil :background "grey90"))
 
   ;; be easy on the eyes
   ;; (if (equal (get-screen-dimensions) '(1440 900))
