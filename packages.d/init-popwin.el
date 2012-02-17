48587a0d1e3724ff1c71e9dae0f8742465153626
Dimitri Fontaine <dim@tapoueh.org>
2012-02-17 13:14:53 +0100

    Enhance popwin setup, fix the browse-kill-ring issue.

--- packages.d/init-popwin.el
+++ packages.d/init-popwin.el
@@ -1,12 +1,16 @@
 ;;; init-popwin.el --- el-get init file for package popwin
 ;;
 
+(require 'popwin-browse-kill-ring nil t)
+
 (loop for b in '("*Ido Completions*"
 		 "*Quail Completions*"
 		 "*Anything Occur*"
 		 "*Disabled Command*"
 		 "*Backtrace*"
-		 "*Kill Ring*")
+		 "*Kill Ring*"
+		 "*cscope*"
+		 "*BBDB*")
       do (add-to-list
 	  'popwin:special-display-config
 	  `(,b :noselect t))
