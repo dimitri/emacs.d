fa0107c56a03bfe3d19f4254caaa75ae56add4de
Dimitri Fontaine <dim@tapoueh.org>
2012-03-12 11:25:34 +0100

    Simplify M-x wget.

--- fun/wget.el
+++ fun/wget.el
@@ -7,21 +7,13 @@
 
 (defun wget (&optional url filename)
   "Download URL into FILENAME"
-  (interactive
-   (list (read-minibuffer "URL: ")
-	 (read-minibuffer "Filename: ")))
+  (interactive "MURL: ")
 
-  (when (file-exists-p filename)
-    (if (yes-or-no-p (format "Replace \"%s\" content? " filename))
-	(delete-file filename)
-      (error "File \"%s\" already exists." filename)))
-
-  (with-temp-file filename
-    (insert
-     (with-current-buffer (url-retrieve-synchronously url)
-       ;; prune HTTP headers
-       (goto-char (point-min))
-       (re-search-forward "^$" nil 'move)
-       (forward-char)
-       (delete-region (point-min) (point))
-       (buffer-string)))))
+  (insert
+   (with-current-buffer (url-retrieve-synchronously url)
+     ;; prune HTTP headers
+     (goto-char (point-min))
+     (re-search-forward "^$" nil 'move)
+     (forward-char)
+     (delete-region (point-min) (point))
+     (buffer-string))))
