67484d922409e6b2f854964449a6b83c724b5549
Dimitri Fontaine <dim@tapoueh.org>
2012-04-16 16:57:25 +0200

    Fix dim-lisp dependencies.

--- dim-lisp.el
+++ dim-lisp.el
@@ -2,7 +2,15 @@
 ;;
 ;; Emacs and Lisp is SLIME, Quicklisp is the easy way here.
 
+(require 'cl)
+(require 'info)
+
 (load (expand-file-name "~/quicklisp/slime-helper.el"))
 (setq inferior-lisp-program "/Users/dim/dev/CL/ccl/dx86cl64 -K utf-8")
+
+(loop for p in '("/Users/dim/dev/CL/dpans2texi-1.05"
+		 "/Users/dim/dev/CL/cl-yacc"
+		 "/Users/dim/dev/CL/asdf/doc")
+      do (add-to-list 'Info-directory-list p))
 
 (provide 'dim-lisp)
