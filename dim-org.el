bee260dea3817c6761dc7e48e042b3aa7c9ece0b
Dimitri Fontaine <dim@tapoueh.org>
2013-01-03 16:50:08 +0100

    Have org-mode give me back my windmove keys, damn it.

--- dim-org.el
+++ dim-org.el
@@ -2,6 +2,16 @@
 
 (require 'org-install)
 
+;; http://orgmode.org/manual/Conflicts.html#Conflicts
+(setq org-replace-disputed-keys t)
+
+;; Make windmove work in org-mode:
+(progn
+  (add-hook 'org-metaup-hook 'windmove-up)
+  (add-hook 'org-metadown-hook 'windmove-down)
+  (add-hook 'org-metaleft-hook 'windmove-left)
+  (add-hook 'org-metaright-hook 'windmove-right))
+
 (setq org-agenda-files '("~/2ndQuadrant/Gestion/Calendrier")
       org-completion-use-ido t
       org-return-follows-link t)
