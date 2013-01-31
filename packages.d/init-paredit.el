e2445d73e264cb14b1ac3e6c9abe80ef1397bafe
Dimitri Fontaine <dim@tapoueh.org>
2013-01-31 23:29:07 +0100

    Fix init-paredit.el to require paredit...

--- packages.d/init-paredit.el
+++ packages.d/init-paredit.el
@@ -1,5 +1,7 @@
 ;;; init-paredit.el --- el-get init file for package paredit
 
+(require 'paredit)
+
 (add-hook 'lisp-mode-hook 'enable-paredit-mode)
 (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
 
