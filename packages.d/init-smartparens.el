12a1c76b2377ba46e86fb9dceaab40ac2e68c582
Dimitri Fontaine <dim@tapoueh.org>
2013-12-30 14:12:13 +0100

    Switch to using smartparens from paredit.

---
+++ packages.d/init-smartparens.el
@@ -0,0 +1,10 @@
+;;; init-smartparens.el --- el-get init file for package smartparens
+
+(require 'smartparens-config)
+(smartparens-strict-mode 1)
+(smartparens-global-mode 1)
+
+;; manually remove that major annoyance.
+(define-key smartparens-mode-map (kbd "M-<up>") 'windmove-up)
+(define-key smartparens-mode-map (kbd "M-<down>") 'windmove-down)
+
